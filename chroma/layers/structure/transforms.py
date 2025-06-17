fac9383600569da030a875f5547c4d9a3cbedd86
arjun-ravi1 <90236206+arjun-ravi1@users.noreply.github.com>
2025-06-17 09:04:41 -0500

    Update transforms.py

--- chroma/layers/structure/transforms.py
+++ chroma/layers/structure/transforms.py
@@ -22,7 +22,6 @@ This module contains pytorch layers for computing and composing with
 from typing import Optional, Tuple
 
 import torch
-import intel_extension_for_pytorch as ipex
 import torch.nn.functional as F
 
 from chroma.layers import graph
