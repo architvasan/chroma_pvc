4aafb6197b6eea8ba09c9a9f7e9f0690c091ae12
arjun-ravi1 <90236206+arjun-ravi1@users.noreply.github.com>
2025-06-17 09:01:05 -0500

    Update backbone.py

--- chroma/layers/structure/backbone.py
+++ chroma/layers/structure/backbone.py
@@ -26,7 +26,6 @@ import numpy as np
 import torch
 import torch.nn as nn
 
-import intel_extension_for_pytorch as ipex
 import torch.nn.functional as F
 
 from chroma.layers.structure import geometry, transforms
