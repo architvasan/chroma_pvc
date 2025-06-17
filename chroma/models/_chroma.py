1d112d0c2d065bff9ec8d0beb76877537a15b07b
arjun-ravi1 <90236206+arjun-ravi1@users.noreply.github.com>
2025-06-17 09:36:25 -0500

    Update _chroma.py

--- chroma/models/_chroma.py
+++ chroma/models/_chroma.py
@@ -23,7 +23,6 @@ from typing import List, Literal, Optional, Tuple, Union
 
 import torch
 import torch.nn as nn
-import intel_extension_for_pytorch as ipex
 from chroma.constants import AA20_3
 from chroma.data.protein import Protein
 from chroma.layers.structure.backbone import ProteinBackbone
